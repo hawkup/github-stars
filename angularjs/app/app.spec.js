7cb6fabe3ff23d3d91b67ba5773a758a44e55764
Tanarat <spiritsquare@gmail.com>
2016-04-12 02:10:16 +0700

    [Angular] Add karma, mocha, chai, sinon, phantomjs for unit test

---
+++ angularjs/app/app.spec.js
@@ -0,0 +1,7 @@
+'use strict';
+
+describe('Unit test', function () {
+  it('true is equal true', function () {
+    expect(true).to.equal(true);
+  });
+});
---
+++ angularjs/karma.conf.js
@@ -0,0 +1,28 @@
+var paths = require('./gulp.config.json');
+
+module.exports = function (config) {
+  var source = [].concat(paths.vendorjs)
+              .concat(paths.js);
+  config.set({
+    basePath: './',
+    files: source,
+    frameworks: ['mocha', 'chai', 'sinon-chai'],
+    browsers: ['PhantomJS'],
+    reporters: ['mocha', 'coverage'],
+    colors: true,
+    plugins: [
+      'karma-phantomjs-launcher',
+      'karma-mocha',
+      'karma-chai-plugins',
+      'karma-mocha-reporter',
+      'karma-coverage'
+    ],
+    preprocessors: {
+      'app/**/*.js': ['coverage']
+    },
+    coverageReporter: {
+      type : 'html',
+      dir : 'coverage/'
+    }
+  })
+}
