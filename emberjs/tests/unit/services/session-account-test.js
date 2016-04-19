3b08432d1f3526f1ddd407d8342d3bfb01eeee23
Tanarat <spiritsquare@gmail.com>
2016-04-19 23:55:02 +0700

    [Ember] Add session-account unit test

--- emberjs/tests/unit/services/session-account-test.js
+++ emberjs/tests/unit/services/session-account-test.js
@@ -1,12 +1,50 @@
 import { moduleFor, test } from 'ember-qunit';
+import Ember from 'ember';
 
-moduleFor('service:session-account', 'Unit | Service | session account', {
-  // Specify the other units that are required for this test.
-  // needs: ['service:foo']
+const { RSVP } = Ember;
+
+moduleFor('service:session-account', 'Unit | Service | session account', {});
+
+test('it should resolved when loadCurrentUser with invalid token', function(assert) {
+  let service = this.subject({
+    session: { data: { authenticated: { access_token: null}}}
+  });
+  service.loadCurrentUser().then(() => {
+    assert.ok(true, 'resolve');
+  });
 });
 
-// Replace this with your real tests.
-test('it exists', function(assert) {
-  let service = this.subject();
-  assert.ok(service);
+test('it should resolved when loadCurrentUser with valid token and can get user', function(assert) {
+  let service = this.subject({
+    session: { data: { authenticated: { access_token: 'token'}}},
+    store: {
+      find() {
+        var mockedModel = Ember.Object.create({
+          'login': 'hawkup'
+        });
+        return new RSVP.Promise((resolve) => {
+          resolve(mockedModel);
+        });
+      }
+    }
+  });
+  service.loadCurrentUser().then(() => {
+    assert.ok(true, 'resolve');
+  });
+});
+
+test('it should resolved when loadCurrentUser with valid token and can not get user', function(assert) {
+  let service = this.subject({
+    session: { data: { authenticated: { access_token: 'token'}}},
+    store: {
+      find() {
+        return new RSVP.Promise((resolve, reject) => {
+          reject();
+        });
+      }
+    }
+  });
+  service.loadCurrentUser().catch(() => {
+    assert.ok(true, 'reject');
+  });
 });
