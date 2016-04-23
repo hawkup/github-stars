d317cae5498b094a0de12e6d816c3321bd54bb4d
Tanarat <spiritsquare@gmail.com>
2016-04-23 22:09:47 +0700

    [Angular] Remove not use sinon on topnav component spec

--- angularjs/app/components/topnav.component.spec.js
+++ angularjs/app/components/topnav.component.spec.js
@@ -6,7 +6,6 @@ describe('Component: topnav', function () {
   var $scope;
   var $compile;
   var element;
-  var sandbox;
 
   beforeEach(module('satellizer'));
   beforeEach(module('ui.router'));
@@ -17,14 +16,9 @@ describe('Component: topnav', function () {
   beforeEach(inject(function ($rootScope, _$componentController_, _$compile_) {
     $scope = $rootScope.$new();
     $compile = _$compile_;
-    sandbox = sinon.sandbox.create();
     $controller = _$componentController_;
     user = {"login":"hawkup","id":2748846,"avatar_url":"https://avatars.githubusercontent.com/u/2748846?v=3","gravatar_id":"","url":"https://api.github.com/users/hawkup","html_url":"https://github.com/hawkup","followers_url":"https://api.github.com/users/hawkup/followers","following_url":"https://api.github.com/users/hawkup/following{/other_user}","gists_url":"https://api.github.com/users/hawkup/gists{/gist_id}","starred_url":"https://api.github.com/users/hawkup/starred{/owner}{/repo}","subscriptions_url":"https://api.github.com/users/hawkup/subscriptions","organizations_url":"https://api.github.com/users/hawkup/orgs","repos_url":"https://api.github.com/users/hawkup/repos","events_url":"https://api.github.com/users/hawkup/events{/privacy}","received_events_url":"https://api.github.com/users/hawkup/received_events","type":"User","site_admin":false,"name":null,"company":null,"blog":null,"location":null,"email":null,"hireable":null,"bio":null,"public_repos":23,"public_gists":51,"followers":7,"following":0,"created_at":"2012-11-08T06:52:46Z","updated_at":"2016-04-01T22:24:06Z"};
   }));
-
-  afterEach(function () {
-    sandbox.restore();
-  });
 
   it('should set loggedIn to be false when user is null', function () {
     var vm = $controller('topnav',
