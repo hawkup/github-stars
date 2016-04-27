6658b63310ddd9f320403ed81165d63abf38c15a
Tanarat <spiritsquare@gmail.com>
2016-04-27 13:35:40 +0700

    [React Redux] Change to use Enzyme for render component on testing

--- reactjs-redux/src/test/components/Counter.spec.js
+++ reactjs-redux/src/test/components/Counter.spec.js
@@ -1,61 +1,56 @@
 import expect from 'expect';
 import React from 'react';
-import TestUtils from 'react-addons-test-utils';
+import { shallow } from 'enzyme';
 import Counter from '../../components/Counter';
 
-function setup() {
-  const props = {
-    counter: {
-      id: 0,
-      count: 0
-    },
+function setup(counter = {id: 0, count: 0}) {
+  const actions = {
     removeCounter: expect.createSpy(),
     increment: expect.createSpy(),
     decrement: expect.createSpy()
   };
-  const renderer = TestUtils.createRenderer();
-  renderer.render(<Counter {...props} />);
-  const output = renderer.getRenderOutput();
+
+  const component = shallow(
+    <Counter counter={counter} {...actions} />
+  );
 
   return {
-    props,
-    output,
-    renderer
+    component,
+    actions,
+    p: component.find('p'),
+    span: component.find('span'),
+    btnIncrement: component.find('button').at(0),
+    btnDecrement: component.find('button').at(1),
+    btnRemove: component.find('button').at(2)
   };
 }
 
 describe('Counter component', () => {
   it('initial render', () => {
-    const { output } = setup();
+    const { p, span, btnIncrement, btnDecrement, btnRemove } = setup();
 
-    expect(output.type).toBe('p');
-
-    const [ span, btnIncrement, btnDecrement, btnRemove ] = output.props.children;
-
-    expect(span.props.children[1]).toBe(0)
-    expect(btnIncrement.props.children).toBe('+');
-    expect(btnDecrement.props.children).toBe('-');
-    expect(btnRemove.props.children).toBe('Remove');
+    expect(p).toExist();
+    expect(span.text()).toBe('Clicked: 0 times');
+    expect(btnIncrement.text()).toBe('+');
+    expect(btnDecrement.text()).toBe('-');
+    expect(btnRemove.text()).toBe('Remove');
   });
 
   it('should increase count when click + button', () => {
-    const { output, props } = setup();
-    const button = output.props.children[1];
-    button.props.onClick({});
-    expect(props.increment).toHaveBeenCalledWith(0);
+    const { btnIncrement, actions } = setup();
+    btnIncrement.simulate('click');
+    expect(actions.increment).toHaveBeenCalledWith(0);
   });
 
   it('should decrease count when click - button', () => {
-    const { output, props } = setup();
-    const button = output.props.children[2];
-    button.props.onClick({});
-    expect(props.decrement).toHaveBeenCalledWith(0);
+    const { btnDecrement, actions } = setup();
+    btnDecrement.simulate('click');
+    expect(actions.decrement).toHaveBeenCalledWith(0);
   });
 
   it('should remove counter when click Remove button', () => {
-    const { output, props } = setup();
-    const button = output.props.children[3];
-    button.props.onClick({});
-    expect(props.removeCounter).toHaveBeenCalledWith(0);
+    const { btnRemove, actions } = setup();
+    btnRemove.simulate('click');
+    expect(actions.removeCounter).toHaveBeenCalledWith(0);
   });
 });
