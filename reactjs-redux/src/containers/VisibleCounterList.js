7a2f73d9924cb1a1d48a01ef636b189d115389b2
Tanarat <spiritsquare@gmail.com>
2016-05-02 22:08:23 +0700

    [React Redux] Move selector out from container

--- reactjs-redux/src/containers/VisibleCounterList.js
+++ reactjs-redux/src/containers/VisibleCounterList.js
@@ -1,26 +1,7 @@
 import { connect } from 'react-redux';
-import { createSelector } from 'reselect';
+import { getVisibleCounters } from '../selectors';
 import { addCounter, removeCounter, increment, decrement } from '../actions';
 import CounterList from '../components/CounterList';
-
-const getVisibilityFilter = (state) => state.visibilityFilter;
-const getCounters = (state) => state.counters;
-
-const getVisibleCounters = createSelector(
-  [ getCounters, getVisibilityFilter ],
-  (counters, filter) => {
-    switch (filter) {
-      case 'SHOW_ALL':
-        return counters;
-      case 'SHOW_ODD':
-        return counters.filter(c => c.count % 2 === 1);
-      case 'SHOW_EVEN':
-        return counters.filter(c => c.count % 2 === 0);
-      default:
-        return counters;
-    }
-  }
-);
 
 const mapStateToProps = (state) => {
   return {
---
+++ reactjs-redux/src/selectors/index.js
@@ -0,0 +1,20 @@
+import { createSelector } from 'reselect';
+
+const getVisibilityFilter = (state) => state.visibilityFilter;
+const getCounters = (state) => state.counters;
+
+export const getVisibleCounters = createSelector(
+  [ getCounters, getVisibilityFilter ],
+  (counters, filter) => {
+    switch (filter) {
+      case 'SHOW_ALL':
+        return counters;
+      case 'SHOW_ODD':
+        return counters.filter(c => c.count % 2 === 1);
+      case 'SHOW_EVEN':
+        return counters.filter(c => c.count % 2 === 0);
+      default:
+        return counters;
+    }
+  }
+);
