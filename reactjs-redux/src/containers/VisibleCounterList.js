6f86170c60c116d7be1d924a494767d4cba7607f
Tanarat <spiritsquare@gmail.com>
2016-05-02 16:26:25 +0700

    [React Redux] Add reselect for getVisibleCounters

--- reactjs-redux/src/containers/VisibleCounterList.js
+++ reactjs-redux/src/containers/VisibleCounterList.js
@@ -1,21 +1,30 @@
 import { connect } from 'react-redux';
+import { createSelector } from 'reselect';
 import { addCounter, removeCounter, increment, decrement } from '../actions';
 import CounterList from '../components/CounterList';
 
-const getVisibleCounter = (counters, filter) => {
-  switch (filter) {
-    case 'SHOW_ALL':
-      return counters;
-    case 'SHOW_ODD':
-      return counters.filter(c => c.count % 2 === 1);
-    case 'SHOW_EVEN':
-      return counters.filter(c => c.count % 2 === 0);
+const getVisibilityFilter = (state) => state.visibilityFilter;
+const getCounters = (state) => state.counters;
+
+const getVisibleCounters = createSelector(
+  [ getCounters, getVisibilityFilter ],
+  (counters, filter) => {
+    switch (filter) {
+      case 'SHOW_ALL':
+        return counters;
+      case 'SHOW_ODD':
+        return counters.filter(c => c.count % 2 === 1);
+      case 'SHOW_EVEN':
+        return counters.filter(c => c.count % 2 === 0);
+      default:
+        return counters;
+    }
   }
-}
+);
 
 const mapStateToProps = (state) => {
   return {
-    counters: getVisibleCounter(state.counters, state.visibilityFilter)
+    counters: getVisibleCounters(state)
   };
 }
 
